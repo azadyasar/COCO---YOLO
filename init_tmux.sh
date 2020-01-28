f6e82916fd0032b2db7b330c493b6843cbb85b55
Azad <ceazadyasar@gmail.com>
2020-01-28 12:27:33 +0300

    updated tmux init. Moved files above drive/

--- init_tmux.sh
+++ init_tmux.sh
@@ -1,3 +1,5 @@
 echo "unbind C-b
 set-option -g prefix C-a
-bind-key C-a send-prefix" > ~/.tmux.conf
+bind-key C-a send-prefixset -g mode-mouse on
+set -g mouse-select-window on
+set -g mouse-select-pane on" > ~/.tmux.conf
