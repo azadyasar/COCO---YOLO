fc8cc64d4dc5b0956eb7718146ec3533c3d28608
Azad <ceazadyasar@gmail.com>
2020-01-28 12:46:40 +0300

    removed print

--- init_tmux.sh
+++ init_tmux.sh
@@ -1,5 +1,6 @@
 echo "unbind C-b
 set-option -g prefix C-a
-bind-key C-a send-prefixset -g mode-mouse on
+bind-key C-a send-prefix
+set -g mode-mouse on
 set -g mouse-select-window on
 set -g mouse-select-pane on" > ~/.tmux.conf
